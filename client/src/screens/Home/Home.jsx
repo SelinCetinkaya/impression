493de8efedec45d49aa745902e66fc258f7bc0db
Selin Cetinkaya <selin.s.cetinkaya@gmail.com>
2021-05-01 16:04:06 -0500

    minor changes to sign-in screen

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -4,7 +4,7 @@ import SignIn from "../../components/SignIn/SignIn";
 
 function Home(props) {
   return (
-    <div>
+    <div className="sign-in-screen">
       <h1>Impression</h1>
       <h3>Share photos and get connected.</h3>
       <SignIn />
